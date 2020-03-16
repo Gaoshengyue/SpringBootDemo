894ef047f79f037ea2069c2ff73c199194889587
symoon <597426245@qq.com>
2020-03-16 10:46:17 +0800

    更改User Model结构

--- src/main/java/com/pyramid/loansupermarket/ApplicationsClass.java
+++ src/main/java/com/pyramid/loansupermarket/ApplicationsClass.java
@@ -6,24 +6,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-//@RestController
 @SpringBootApplication
 public class ApplicationsClass {
 
 
     @RequestMapping("/")
-    public String index(ModelMap map){
-        map.addAttribute("newt","symoon");
+    public String index(ModelMap map) {
+        map.addAttribute("newt", "symoon");
         return "index";
     }
 
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
-        SpringApplication.run(ApplicationsClass.class,args);
+        SpringApplication.run(ApplicationsClass.class, args);
     }
 }
 
--- src/main/java/com/pyramid/loansupermarket/model/User.java
+++ src/main/java/com/pyramid/loansupermarket/model/User.java
@@ -1,37 +1,73 @@
 package com.pyramid.loansupermarket.model;
+
 import javax.persistence.*;
+
 import lombok.Data;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Data
 @Entity
-@Table(name="user")
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  int id;
-    @Column(name="username")
+    private int id;
+    @Column(name = "username")
     private String username;
 
     @Column(name = "password")
     private String password;
 
-    public void setUsername(String username){
-        this.username=username;
-    }
-    public void setPassword(String password){
+    @Column(name="phone_number")
+    private String phoneNumber;
 
-        this.password=password;
+    @Column(name="real_name")
+    private String realName;
+
+    @Column(name="IDNumber")
+    private String idNumber;
+
+
+    public void setUsername(String username) {
+        this.username = username;
     }
-    public Integer getId(){
+
+    public void setPassword(String password) {
+
+        this.password = password;
+    }
+    public void setPhoneNumber(String phoneNumber){
+
+        this.phoneNumber= phoneNumber;
+    }
+
+    public void setRealName(String realName){
+        this.realName=realName;
+    }
+    public void setIdNumber(String idNumber){
+        this.idNumber = idNumber;
+    }
+
+    public Integer getId() {
 
         return id;
     }
-    public String getUsername(){
+
+    public String getUsername() {
         return username;
     }
-    public String getPassword(){
+
+    public String getPassword() {
         return password;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public String getRealName(){
+        return realName;
+    }
+    public String getIdNumber(){
+        return idNumber;
     }
 
 }
